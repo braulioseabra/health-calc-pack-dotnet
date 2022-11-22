c5467696d092ddcfa5bb4ff7d5a09558651ce72e
braulioseabra <braulio.seabra@gmail.com>
2022-11-22 07:36:31 -0300

    Commit realizado para refletir a configuracao da Console: health-calc-console-dotnet.

---
+++ health-calc-pack-dotnet-braulio-seabra/src/health-calc-pack-dotnet-braulio-seabra/health-calc-console-dotnet/Program.cs
@@ -0,0 +1,20 @@
+﻿using health_calc_pack_dotnet_braulio_seabra;
+
+Console.WriteLine("Entre com a sua altura e o seu peso para calcular o seu IMC!");
+
+Console.Write("Altura: ");
+var Height = Console.ReadLine();
+
+Console.Write("Peso: ");
+var Weight = Console.ReadLine();
+
+IMC objIMC = new IMC();
+
+double Result = objIMC.Calc(double.Parse(Height), double.Parse(Weight));
+
+string Category = objIMC.GetIMCCategory(Result);
+
+
+Console.WriteLine(" O resultado do seu IMC foi: " + Category);
+
+Console.ReadKey();
